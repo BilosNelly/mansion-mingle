542ac06b2c352e95817595671da39fe6a5c2452f
BilosNelly <bilosantonela1996@gmail.com>
2023-01-21 19:57:01 +0100

    Working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
 
-function App() {
+export const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [propertyData, setPropertyData] = useState<{ property: Array<{id:number, title:string, description:string, address:Array<{country:string, city:string, street:string, zipCode:string}>, picture:string}> }>({property:[]});
+  const [filteredProperties, setFilteredProperties] = useState(propertyData.property)
+
+  useEffect(() => {
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        setPropertyData(result);
+        setFilteredProperties(result.property);
+      });
+  }, []);
+
+  const handleSearch = () => {
+    if(searchTerm){
+    const filteredData = propertyData.property.filter(property => {
+      return property.title.toLowerCase().includes(searchTerm.toLowerCase()) || property.description.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setFilteredProperties(filteredData);
+    } else{
+      setFilteredProperties(propertyData.property)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <div className="homepage__searchbar">
+        <div className="homepage__searchbar__title">What are you searching for?</div>
+        <input type="text" placeholder="Search by title or description" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="homepage__wrapper">
+          {filteredProperties.length === 0 ? <div>No properties found with the search criteria</div> : filteredProperties.map((element, index) => (
+            <ul className="homepage__list">
+            <li key={index} className="homepage__list__item">
+              <img src={element.picture} />
+              <div>Property title: {element.title}</div>
+              <div>Description: {element.description}</div>
+            </li>
+            </ul>
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
